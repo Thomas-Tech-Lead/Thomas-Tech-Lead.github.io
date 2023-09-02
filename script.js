cd93ad1b1a005657c2ddcd4f59a38e1d26f6c787
Thomas-Tech-Lead <83269968+Thomas-Tech-Lead@users.noreply.github.com>
2023-09-02 01:20:11 -0700

    Header Updated
    In mobile view, the header will be hidden when scroll down even when navbar is displayed. navbar will also be hidden on scroll down event.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var header = document.getElementById("header");
 window.addEventListener("scroll", function(){
     var scrollTop = this.window.pageYOffset || this.document.documentElement.scrollTop;
     if (scrollTop > lastScrollTop) {
-        header.style.top = "-4.6rem";
+        header.style.top = "-15.3rem";
     } else {
         header.style.top = "0";
     }
